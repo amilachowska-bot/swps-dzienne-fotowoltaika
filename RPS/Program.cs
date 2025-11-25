0797b32f7d03d6a9ca9462d917ac3b58c663b5bb
elojelo <amilachowska@st.swps.edu.pl>
2025-11-25 11:11:28 +0100

    cleanup

--- RPS/Program.cs
+++ RPS/Program.cs
@@ -4,7 +4,7 @@ string GetCorrectSign(string playerName)
     Console.WriteLine($"{playerName}, choose your sign ({string.Join('/', allowedSigns)})");
     string sign = Console.ReadLine()!;
 
-    // while (!(firstSign.Equals(allowedSigns[0], stringComparison) || firstSign.Equals(allowedSigns[1], stringComparison) || firstSign.Equals(allowedSigns[2], stringComparison)))
+    
     while (!allowedSigns.Contains(sign, StringComparer.OrdinalIgnoreCase))
     {
         Console.WriteLine($"{playerName}, choose correct sign ({string.Join('/', allowedSigns)})");
@@ -18,25 +18,15 @@ const StringComparison stringComparison = StringComparison.OrdinalIgnoreCase;
 
 Console.WriteLine("Let's play Rock-Paper-Scissors!");
 
-// Console.WriteLine($"Player 1, choose your sign ({string.Join('/', allowedSigns)})");
-// Console.WriteLine($"Player 1, choose your sign ({string.Join("/", allowedSigns)})");
-// Console.WriteLine($"Player 1, choose your sign ({allowedSigns[0]}/{allowedSigns[1]}/{allowedSigns[2]})");
-// string firstSign = Console.ReadLine()!;
 
-// while (!(firstSign.Equals(allowedSigns[0], stringComparison) || firstSign.Equals(allowedSigns[1], stringComparison) || firstSign.Equals(allowedSigns[2], stringComparison)))
-// while (!allowedSigns.Contains(firstSign, StringComparer.OrdinalIgnoreCase))
-// {
-//     Console.WriteLine($"Player 1, choose correct sign ({string.Join('/', allowedSigns)})");
-//     firstSign = Console.ReadLine()!;
-// }
 string firstSign = GetCorrectSign("Player 1");
 string secondSign = GetCorrectSign("Player 2");
 
-// 1. Pobierz indeks znaku podanego przez osobę drugą (np. 0, 1, 2) - nazwę to secondSignIndex
+
 int secondSignIndex = allowedSigns.IndexOf(secondSign);
-// 2. Wylicz indeks znaku, który wygrywa z podanym przez osobę drugą - wzór - (secondSignIndex + 1) % rozmiarListy
+
 int winningSignIndex = (secondSignIndex + 1) % allowedSigns.Count;
-// 3. Czy indeks znaku podanego przez pierwszą osobę to indeks wyliczony w punkcie 2.
+
 int firstSignIndex = allowedSigns.IndexOf(firstSign);
 
 
